31ffe4fded8d59f42bbd521416a35cc810d74c0f
Lucas Pennice <lucaspennice@gmail.com>
2022-07-16 22:11:23 -0300

    backend url

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'http://localhost:3200/api';
+// export const BACKEND_URL = 'http://localhost:3200/api';
+export const BACKEND_URL = 'https://faltazo-server.vercel.app/api';
