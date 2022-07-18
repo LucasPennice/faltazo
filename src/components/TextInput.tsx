6dcb06d61b831c1d2014ba83802f279f773be216
Lucas Pennice <lucaspennice@gmail.com>
2022-07-18 20:34:12 -0300

    finished create profile form

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,23 +1,9 @@
-type propType = {
-	id: string;
-	data: { value: string; setter: (value: string) => void };
-};
-
-const TextInput = ({ id, data }: propType) => {
-	const { setter, value } = data;
+const TextInput = () => {
 	return (
-		<>
-			<label htmlFor={id} className='opacity-30'>
-				{id.toUpperCase()}
-			</label>
-			<input
-				type='text'
-				id={id}
-				value={value}
-				onChange={(e) => setter(e.target.value)}
-				className='border border-gray-300 focus:border-purple-800 transition-all p-1 rounded focus:outline-none'
-			/>
-		</>
+		<input
+			type='text'
+			className='border border-gray-300 focus:border-purple-800 transition-all p-1 rounded focus:outline-none'
+		/>
 	);
 };
 export default TextInput;
--- src/pages/CreateProfilet.tsx
+++ src/pages/CreateProfilet.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
-import TextArea from '../components/TextArea';
 import { motion as m } from 'framer-motion';
-import TextInput from '../components/TextInput';
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
@@ -12,37 +10,12 @@ import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 import { toast, ToastContainer } from 'react-toastify';
 import axios from 'axios';
 import { BACKEND_URL } from '../utils';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 registerPlugin(FilePondPluginFileValidateType, FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 const CreateProfile = () => {
-	const [userName, setUserName] = useState('');
-	const [userDescription, setUserDescription] = useState('');
-	const [userBio, setUserBio] = useState('');
 	const [files, setFiles] = useState<any>([]);
 	const [fileFinishedUploading, setfileFinishedUploading] = useState<boolean>(false);
-
-	const handleSubmit = async (e: any) => {
-		e.preventDefault();
-		if (!userName) return toast.warning('Username Empty');
-		if (!userDescription) return toast.warning('User description empty');
-		if (!userBio) return toast.warning('user bio empty');
-		if (!fileFinishedUploading) return toast.warning('Please wait for the file to finish uploading');
-		if (files.length === 0) return toast.warning('Upload a pic');
-		const response = await toast.promise(
-			() => axios.post(`${BACKEND_URL}/profile`, { userName, userDescription, userBio }),
-			{
-				pending: 'Loading',
-				success: {
-					render() {
-						console.log('teas');
-						return `Profile created`;
-					},
-				},
-				error: 'There was an error',
-			}
-		);
-		console.log(response);
-	};
 
 	return (
 		<div className='w-full flex justify-center'>
@@ -50,37 +23,81 @@ const CreateProfile = () => {
 				<m.h1 className='mb-10' initial={{ opacity: 0, fontSize: '1rem' }} animate={{ opacity: 1, fontSize: '2rem' }}>
 					CREAR PERFIL
 				</m.h1>
-				<m.form
-					initial={{ opacity: 0, y: 50 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ delay: 0.2 }}
-					onSubmit={handleSubmit}
-					className='w-full px-8'>
-					<section className='flex-col flex gap-3 mb-8'>
-						<FilePond
-							files={files}
-							onupdatefiles={setFiles}
-							allowMultiple={true}
-							onprocessfile={() => setfileFinishedUploading(true)}
-							acceptedFileTypes={['image/png', 'image/jpg', 'image/jpeg']}
-							maxFiles={3}
-							server={`${BACKEND_URL}/uploadProfilePicture`}
-							name='files'
-							labelIdle={`Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`}
-						/>
+				<Formik
+					initialValues={{ profileName: '', profileBio: '' }}
+					validate={(values) => {
+						const errors: any = {};
+						if (!values.profileName) {
+							errors.profileName = 'Required';
+						}
+						if (!values.profileBio) {
+							errors.profileBio = 'Required';
+						}
+						return errors;
+					}}
+					onSubmit={async (values, { setSubmitting }) => {
+						if (files.length === 0) {
+							toast.warning('Upload a pic');
+							setSubmitting(false);
+							return;
+						}
+						if (!fileFinishedUploading) {
+							toast.warning('Please wait for the file to finish uploading');
+							setSubmitting(false);
+							return;
+						}
+						const response = await toast.promise(() => axios.post(`${BACKEND_URL}/profile`, values), {
+							pending: 'Loading',
+							success: {
+								render() {
+									console.log(values);
+									setSubmitting(false);
+									return `Profile created`;
+								},
+							},
+							error: 'There was an error',
+						});
+						alert('reload the website for the user');
+						console.log(response);
+					}}>
+					{({ isSubmitting }) => (
+						<Form className='flex flex-col w-full gap-7'>
+							<FilePond
+								files={files}
+								onupdatefiles={setFiles}
+								allowMultiple={true}
+								onprocessfile={() => setfileFinishedUploading(true)}
+								acceptedFileTypes={['image/png', 'image/jpg', 'image/jpeg']}
+								maxFiles={3}
+								server={`${BACKEND_URL}/uploadSingleFile`}
+								name='fileName'
+								labelIdle={`Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`}
+							/>
+							<Field
+								type='text'
+								name='profileName'
+								className='border border-gray-300 focus:border-purple-800 transition-all p-1 rounded focus:outline-none'
+							/>
+							<ErrorMessage name='profileName' component='div' />
+							<Field
+								type='text'
+								name='profileBio'
+								className='border border-gray-300 focus:border-purple-800 transition-all p-1 rounded focus:outline-none'
+							/>
+							<ErrorMessage name='profileBio' component='div' />
+							<m.button
+								type='submit'
+								disabled={isSubmitting}
+								initial={{ opacity: 0, y: 50 }}
+								animate={{ opacity: 1, y: 0 }}
+								transition={{ delay: 0.4 }}
+								className='gradient w-full py-3 rounded-3xl shadow-lg shadow-purple-500/50 text-white text-xl uppercase tracking-widest'>
+								crear
+							</m.button>
+						</Form>
+					)}
+				</Formik>
 
-						<TextInput id='username' data={{ value: userName, setter: setUserName }} />
-						<TextInput id='description' data={{ value: userDescription, setter: setUserDescription }} />
-						<TextArea id='bio' data={{ value: userBio, setter: setUserBio }} />
-					</section>
-					<m.button
-						initial={{ opacity: 0, y: 50 }}
-						animate={{ opacity: 1, y: 0 }}
-						transition={{ delay: 0.4 }}
-						className='gradient w-full py-3 rounded-3xl shadow-lg shadow-purple-500/50 text-white text-xl uppercase tracking-widest'>
-						crear
-					</m.button>
-				</m.form>
 				<ToastContainer position={'top-center'} theme={'dark'} />
 			</main>
 		</div>
